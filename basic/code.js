8e00629085d1a4f12471c82f01579da53c84df5e
magnus <magnusmichaelson@protonmail.com>
2024-10-26 23:28:09 +0100

    report added

--- basic/code.js
+++ basic/code.js
@@ -1,51 +1,111 @@
 "use strict";
 //
-//
-//
-//
-//
-//
 // code for script include - start
 //
-var Moomin = Class.create();
-Moomin.prototype = {
-  // is this needed?
+var ThingThatNeedsNaming = Class.create();
+ThingThatNeedsNaming.prototype = {
+  //
+  // this might need to be uncommented if this code is run in an actual script include
   // initialize: function() {},
-  main: function (encodedQuery) {
-    //
+  //
+  engage: function (encodedQuery) {
+    // there is only one function, engage()
+    // it will always be called this
+    // all datastructures will exist in the scope of engage()
+    // all functions will exist in the scope of engage()
+    // data will be returned from the scope of engage()
     //
     var badData = {};
     var goodData = {};
-    // nonsense code for testing
-    badData['0001'] = encodedQuery;
-    goodData['0001'] = encodedQuery;
-    // standard return of badData and goodData
+    //
+    var main = function () {
+      //
+      var fakeSysId1 = 'd317aed433a95210702121c91e5c7aaa';
+      var fakeSysId2 = 'd317aed433a95210702121c91e5c7aab';
+      var fakeSysId3 = 'd317aed433a95210702121c91e5c7aac';
+      var fakeSysId4 = 'd317aed433a95210702121c91e5c7aad';
+      var urlStart = '';
+      //
+      urlStart = gs.getProperty('glide.servlet.uri');
+      badData[fakeSysId1] = {
+        failures: {
+          'string_field issue': true,
+          'puny god': true,
+        },
+        data: {
+          number_field: 5,
+          string_field: null,
+        },
+        url: "".concat(urlStart, "alm_hardware.do?sys_id=").concat(fakeSysId1),
+      };
+      badData[fakeSysId2] = {
+        failures: {
+          'number_field issue': true,
+        },
+        data: {
+          number_field: null,
+          string_field: "foo_".concat(encodedQuery),
+        },
+        url: "".concat(urlStart, "alm_hardware.do?sys_id=").concat(fakeSysId2),
+      };
+      //
+      goodData[fakeSysId3] = {
+        number_field: 4,
+        string_field: "foo_".concat(encodedQuery),
+      };
+      goodData[fakeSysId4] = {
+        number_field: 0,
+        string_field: "blah_".concat(encodedQuery),
+      };
+      //
+    };
+    //
+    main();
+    //
     return {
       badData: badData,
       goodData: goodData,
     };
     //
-    //
   },
-  // is this needed?
-  // type: 'Test'
+  type: 'Test',
 };
 //
 // code for script include - end
 //
-//
-//
-//
-//
-//
-var testScriptInclude = function () {
+var report = function (countBad, countGood) {
   //
+  var percentage = 0;
+  var reportString = '';
+  var total = 0;
   //
-  var snorkMaiden = new Moomin();
-  var allData = snorkMaiden.main('nameSTARTSWITHtesting');
-  gs.debug(allData.badData);
-  gs.debug(allData.goodData);
+  total = countBad + countGood;
+  percentage = (countGood / total) * 100;
   //
-  //
+  reportString += '\n\n';
+  reportString += "Total good data = ".concat(countGood, "\n");
+  reportString += "Total bad data = ".concat(countBad, "\n");
+  reportString += "Pass rate = ".concat(percentage, "%\n");
+  reportString += '\n\n';
+  gs.print(reportString);
 };
-testScriptInclude();
+var testCode = function () {
+  //
+  var countBad = 0;
+  var countGood = 0;
+  var badData = null;
+  var goodData = null;
+  //
+  var kaiju = new ThingThatNeedsNaming();
+  var results = kaiju.engage('nameSTARTSWITHtesting');
+  badData = results.badData;
+  goodData = results.goodData;
+  if (badData !== null && goodData !== null) {
+    countBad = Object.keys(badData).length;
+    countGood = Object.keys(goodData).length;
+    report(countBad, countGood);
+  }
+  gs.debug(results.goodData);
+  gs.debug(results.badData);
+};
+testCode();
--- basic/code.ts
+++ basic/code.ts
@@ -1,53 +1,139 @@
 //
-//
-//
-//
-//
-//
 // code for script include - start
 //
 // @ts-ignore
-const Moomin = Class.create();
-Moomin.prototype = {
-  // is this needed?
+const ThingThatNeedsNaming = Class.create();
+ThingThatNeedsNaming.prototype = {
+  //
+  // this might need to be uncommented if this code is run in an actual script include
   // initialize: function() {},
-  main: (encodedQuery: string) => {
+  //
+  engage: (encodedQuery: string) => {
+    // there is only one function, engage()
+    // it will always be called this
+    // all datastructures will exist in the scope of engage()
+    // all functions will exist in the scope of engage()
+    // data will be returned from the scope of engage()
     //
+    const badData: Record<string, {
+      data: {
+        number_field: number | null,
+        string_field: string | null,
+      },
+      failures: Record<string, boolean>,
+      url: string,
+    }> = {};
+    const goodData: Record<string, {
+      number_field: number,
+      string_field: string,
+    }> = {};
     //
-    const badData: Record<string, string> = {};
-    const goodData: Record<string, string> = {};
-    // nonsense code for testing
-    badData['0001'] = encodedQuery;
-    goodData['0001'] = encodedQuery;
-    // standard return of badData and goodData
+    const main = () => {
+      //
+      const fakeSysId1 = 'd317aed433a95210702121c91e5c7aaa';
+      const fakeSysId2 = 'd317aed433a95210702121c91e5c7aab';
+      const fakeSysId3 = 'd317aed433a95210702121c91e5c7aac';
+      const fakeSysId4 = 'd317aed433a95210702121c91e5c7aad';
+      let urlStart = '';
+      //
+      // @ts-ignore
+      urlStart = gs.getProperty('glide.servlet.uri');
+      badData[fakeSysId1] = {
+        failures: {
+          'string_field issue': true,
+          'puny god': true,
+        },
+        data: {
+          number_field: 5,
+          string_field: null,
+        },
+        url: `${urlStart}alm_hardware.do?sys_id=${fakeSysId1}`,
+      };
+      badData[fakeSysId2] = {
+        failures: {
+          'number_field issue': true,
+        },
+        data: {
+          number_field: null,
+          string_field: `foo_${encodedQuery}`,
+        },
+        url: `${urlStart}alm_hardware.do?sys_id=${fakeSysId2}`,
+      };
+      //
+      goodData[fakeSysId3] = {
+        number_field: 4,
+        string_field: `foo_${encodedQuery}`,
+      };
+      goodData[fakeSysId4] = {
+        number_field: 0,
+        string_field: `blah_${encodedQuery}`,
+      };
+      //
+    };
+    //
+    main();
+    //
     return {
       badData,
       goodData,
     };
     //
-    //
   },
-  // is this needed?
-  // type: 'Test'
+  type: 'Test',
 };
 //
 // code for script include - end
 //
-//
-//
-//
-//
-//
-const testScriptInclude = () => {
+const report = (
+  countBad: number,
+  countGood: number,
+) => {
   //
+  let percentage = 0;
+  let reportString = '';
+  let total = 0;
   //
-  const snorkMaiden = new Moomin();
-  const allData = snorkMaiden.main('nameSTARTSWITHtesting');
+  total = countBad + countGood;
+  percentage = (countGood / total) * 100;
+  //
+  reportString += '\n\n';
+  reportString += `Total good data = ${countGood}\n`;
+  reportString += `Total bad data = ${countBad}\n`;
+  reportString += `Pass rate = ${percentage}%\n`;
+  reportString += '\n\n';
   // @ts-ignore
-  gs.debug(allData.badData);
-  // @ts-ignore
-  gs.debug(allData.goodData);
-  //
-  //
+  gs.print(reportString);
 };
-testScriptInclude();
+const testCode = () => {
+  //
+  let countBad = 0;
+  let countGood = 0;
+
+  let badData: Record<string, {
+    data: {
+      number_field: number | null;
+      string_field: string | null;
+    },
+    failures: Record<string, boolean>,
+    url: string,
+  }> | null = null;
+  let goodData: Record<string, {
+    number_field: number;
+    string_field: string;
+  }> | null = null;
+  //
+  const kaiju = new ThingThatNeedsNaming();
+  const results = kaiju.engage('nameSTARTSWITHtesting');
+  badData = results.badData;
+  goodData = results.goodData;
+  if (badData !== null && goodData !== null) {
+    countBad = Object.keys(badData).length;
+    countGood = Object.keys(goodData).length;
+    report(countBad, countGood);
+  }
+  // @ts-ignore
+  gs.debug(results.goodData);
+  // @ts-ignore
+  gs.debug(results.badData);
+};
+testCode();
