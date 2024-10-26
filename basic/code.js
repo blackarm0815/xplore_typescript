92d19a50caece7fec58fa35a21695f2ee639b17b
magnus <magnusmichaelson@protonmail.com>
2024-10-26 13:48:06 +0100

    basic - new layout

--- basic/code.js
+++ basic/code.js
@@ -1,32 +1,51 @@
 "use strict";
-// code for script include
+//
+//
+//
+//
+//
+//
+// code for script include - start
+//
 var Moomin = Class.create();
 Moomin.prototype = {
   // is this needed?
   // initialize: function() {},
-  everything: function (encodedQuery) {
+  main: function (encodedQuery) {
+    //
+    //
     var badData = {};
     var goodData = {};
-    // nonsense code
-    badData[encodedQuery] = 'dogwater';
-    goodData[encodedQuery] = 'sparkles';
+    // nonsense code for testing
+    badData['0001'] = encodedQuery;
+    goodData['0001'] = encodedQuery;
+    // standard return of badData and goodData
     return {
       badData: badData,
       goodData: goodData,
     };
+    //
+    //
   },
   // is this needed?
   // type: 'Test'
 };
 //
+// code for script include - end
 //
 //
 //
-// code for testing in xplore
-var main = function () {
+//
+//
+//
+var testScriptInclude = function () {
+  //
+  //
   var snorkMaiden = new Moomin();
-  var allData = snorkMaiden.everything('nameSTARTSWITHtesting');
+  var allData = snorkMaiden.main('nameSTARTSWITHtesting');
   gs.debug(allData.badData);
   gs.debug(allData.goodData);
+  //
+  //
 };
-main();
+testScriptInclude();
--- basic/code.ts
+++ basic/code.ts
@@ -1,34 +1,53 @@
-// code for script include
+//
+//
+//
+//
+//
+//
+// code for script include - start
+//
 // @ts-ignore
 const Moomin = Class.create();
 Moomin.prototype = {
   // is this needed?
   // initialize: function() {},
-  everything: (encodedQuery: string) => {
+  main: (encodedQuery: string) => {
+    //
+    //
     const badData: Record<string, string> = {};
     const goodData: Record<string, string> = {};
-    // nonsense code
-    badData[encodedQuery] = 'dogwater';
-    goodData[encodedQuery] = 'sparkles';
+    // nonsense code for testing
+    badData['0001'] = encodedQuery;
+    goodData['0001'] = encodedQuery;
+    // standard return of badData and goodData
     return {
       badData,
       goodData,
     };
+    //
+    //
   },
   // is this needed?
   // type: 'Test'
 };
 //
+// code for script include - end
 //
 //
 //
-// code for testing in xplore
-const main = () => {
+//
+//
+//
+const testScriptInclude = () => {
+  //
+  //
   const snorkMaiden = new Moomin();
-  const allData = snorkMaiden.everything('nameSTARTSWITHtesting');
+  const allData = snorkMaiden.main('nameSTARTSWITHtesting');
   // @ts-ignore
   gs.debug(allData.badData);
   // @ts-ignore
   gs.debug(allData.goodData);
+  //
+  //
 };
-main();
+testScriptInclude();
